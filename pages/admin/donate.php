e5410dd56ebab951f7bd4e6adec02ff732d5cd53
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-08-09 07:33:01 +0200

    Style and interface

---
+++ pages/admin/donate.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ERC | Gisenyi</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="box">
+            <div class="special">
+                <header class="flex justify-content-between">
+                    <h2>Donate</h2>
+                    <img src="../../assets/img/diane.jpg" alt="Logo" class="profil">
+                </header>
+                <section class="donate-list">
+                    <p class="flex justify-content-between">
+                        <span>Bank of Kigali:</span>
+                        <span>525-655-098-123</span>
+                    </p>
+                    <p class="flex justify-content-between">
+                        <span>Equity Bank:</span>
+                        <span>552-655-098-012</span>
+                    </p>
+                    <p class="flex justify-content-between">
+                        <span>Momo pay:</span>
+                        <span>*182*8*1*1234567#</span>
+                    </p>
+                    <p class="flex justify-content-between">
+                        <span>Airtel money:</span>
+                        <span>+250 784 545 831</span>
+                    </p>
+                </section>
+                <button class="btn btn-block btn-primary">Donate</button>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ pages/admin/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ERC | Gisenyi</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <a href="index.html">ERC | Gisenyi</a>
+        </div>
+
+        <div class="links">
+            <ul>
+                <li><a href="" class="active">Home</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">Subscribes</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Donate</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="wrapper">
+        <div class="box">
+            <div class="card">
+                <div class="cardHeader">
+                    <img src="./assets/img/jean-pierre14.jpeg" alt="news" class="img-max">
+                </div>
+                <div class="cardBody">
+                    <h2>Les jeunes de Gisenyi vont visite les freres qui sont dans les prisons</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit eum voluptatum ipsam rem qui officiis obcaecati voluptatibus quae expedita id, aperiam doloremque eius excepturi, sit quaerat nulla fuga repellendus? Iusto?
+                    </p>
+                </div>
+                <div class="cardFooter">
+                    <div class="">
+                        <button class="btn btn-primary like">like</button>
+                        <button class="btn btn-danger dislike">dislike</button>
+                    </div>
+                    <div class="">
+                        <img title="Rodrigue masengo" src="./assets/img/bisimwa.jpeg" alt="poster profil">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card">
+
+                <div class="cardBody">
+                    <h2>Les femmes sont dans une grande fete ce soir...</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit eum voluptatum ipsam rem qui officiis obcaecati voluptatibus quae expedita id, aperiam doloremque eius excepturi, sit quaerat nulla fuga repellendus? Iusto?
+                    </p>
+                </div>
+                <div class="cardFooter">
+                    <div class="">
+                        <button class="btn btn-primary like">like</button>
+                        <button class="btn btn-danger dislike">dislike</button>
+                    </div>
+                    <div class="">
+                        <img title="Rodrigue masengo" src="./assets/img/rodrigue.jpg" alt="poster profil">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="card">
+                <div class="cardHeader">
+                    <img src="./assets/img/student.png" alt="news" class="img-max">
+                </div>
+                <div class="cardBody">
+                    <h2>Title</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit eum voluptatum ipsam rem qui officiis obcaecati voluptatibus quae expedita id, aperiam doloremque eius excepturi, sit quaerat nulla fuga repellendus? Iusto?
+                    </p>
+                </div>
+                <div class="cardFooter">
+                    <div class="">
+                        <button class="btn btn-primary like">like</button>
+                        <button class="btn btn-danger dislike">dislike</button>
+                    </div>
+                    <div class="">
+                        <img title="Rodrigue masengo" src="./assets/img/rodrigue.jpg" alt="poster profil">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper">
+        <div class="box">
+            <h3>News</h3>
+            <form action="#" class="myForm">
+                <div class="error-text">This error</div>
+                <div class="group">
+                    <label for="title">Title</label>
+                    <input type="text" id="title" name="title" placeholder="Title" class="input">
+                </div>
+                <div class="group">
+                    <label for="sub-title">Sub-Title</label>
+                    <input type="text" name="title" id="sub-title" placeholder="Title" class="input">
+                </div>
+                <div class="group">
+                    <label for="content">Content</label>
+                    <textarea name="content" id="content" class="textarea" placeholder="Message..."></textarea>
+                </div>
+                <div class="group">
+                    <label for="img">Image</label>
+                    <input type="file" name="img" id="img">
+                </div>
+                <div class="group">
+                    <button>Send <i class="fa fa-send"></i></button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="footer">
+        <div>
+            <h3>Departments</h3>
+            <ul>
+                <li>Evangelisation</li>
+                <li>Evangelisation</li>
+                <li>Evangelisation</li>
+                <li>Evangelisation</li>
+            </ul>
+        </div>
+        <div>
+            <h3>Partners</h3>
+            <ul>
+                <li>ERC | Kigali</li>
+                <li>ERC | Kigali</li>
+                <li>ERC | Kigali</li>
+                <li>ERC | Kigali</li>
+            </ul>
+        </div>
+        <div>
+            <form class="abonnement" action="#">
+                <div class="group">
+                    <label for="email">Subscribe</label>
+                    <input type="email" name="email" id="email" placeholder="Exemple: email@gmail.com" class="input">
+                    <button type="button">Subscribe</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
