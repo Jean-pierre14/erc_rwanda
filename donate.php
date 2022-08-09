b1160a402f2dd1f78d06d84a97e7010104cebd31
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-08-09 07:53:38 +0200

    Structure of the website

---
+++ donate.php
@@ -0,0 +1,6 @@
+<?php include_once "./includes/header.php";?>
+<?php include_once "./includes/navbar.php";?>
+
+<?php include_once "./pages/admin/donate.php";?>
+
+<?php include_once "./includes/footer.php";?>
---
+++ includes/navbar.php
@@ -0,0 +1,15 @@
+<nav>
+        <div class="logo">
+            <a href="index.html">ERC | Gisenyi</a>
+        </div>
+
+        <div class="links">
+            <ul>
+                <li><a href="" class="active">Home</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">Subscribes</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Donate</a></li>
+            </ul>
+        </div>
+    </nav>
